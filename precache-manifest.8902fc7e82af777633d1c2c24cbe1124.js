ffdf534bc81c53f85ff55ab4ec1283f6b3b8e7f5
SarahBechi <59558883+SarahBechi@users.noreply.github.com>
2020-02-17 11:21:36 +0100

    Updates

---
+++ precache-manifest.8902fc7e82af777633d1c2c24cbe1124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905f21e5e454c302e87fd9447deba7dd",
+    "url": "/hoc-with-spinner/index.html"
+  },
+  {
+    "revision": "55969073cada7a91d904",
+    "url": "/hoc-with-spinner/static/css/main.0fa7eb6e.chunk.css"
+  },
+  {
+    "revision": "abbc60ee8e7e92cd45d9",
+    "url": "/hoc-with-spinner/static/js/2.8020639b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hoc-with-spinner/static/js/2.8020639b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55969073cada7a91d904",
+    "url": "/hoc-with-spinner/static/js/main.f92176fa.chunk.js"
+  },
+  {
+    "revision": "b0cfafecda2fb0c4c768",
+    "url": "/hoc-with-spinner/static/js/runtime-main.89f8e273.js"
+  }
+]);
